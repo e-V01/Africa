1bf1420322f270e5bb3ddbb39a0634ed84b242bc
e-V01 <91158407+e-V01@users.noreply.github.com>
2024-01-24 18:32:15 +0200

    json extension decoder func added and data decoded to main tabView

--- Africa/App/ContentView.swift
+++ Africa/App/ContentView.swift
@@ -9,13 +9,19 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Content!")
+            NavigationView {
+                List {
+                    CoverImageView()
+                        .frame(height: 300)
+                        .listRowInsets(EdgeInsets(top: 0, 
+                                                  leading: 0,
+                                                  bottom: 0,
+                                                  trailing: 0))
+                
+            }
+                .navigationTitle("Africa")
+                .navigationBarTitleDisplayMode(.large)
         }
-        .padding()
     }
 }
 
