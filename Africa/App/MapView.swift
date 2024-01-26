a957fe6e55cccc1c7fbca2991fa1b8dc36d800d9
e-V01 <91158407+e-V01@users.noreply.github.com>
2024-01-26 15:12:18 +0200

    locationModel created data decoded to MapView State prop added to bind future changes between screens

--- Africa/App/MapView.swift
+++ Africa/App/MapView.swift
@@ -6,10 +6,35 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct MapView: View {
+    @State private var region: MKCoordinateRegion = {
+        var mapCoordinates = CLLocationCoordinate2D(latitude: 6.600286, longitude: 16.4377599)
+        var mapZoomLevel = MKCoordinateSpan(latitudeDelta: 70.0, longitudeDelta: 70.0)
+        var mapRegion = MKCoordinateRegion(center: mapCoordinates, span: mapZoomLevel)
+        
+        return mapRegion
+    }()
+    
+    let locations: [NationalParkLocation] = Bundle.main.decode("locations.json")
     var body: some View {
-        Text("Map")
+//        Map(coordinateRegion: $region)
+        
+        Map(coordinateRegion: $region, annotationItems: locations, annotationContent: { item in
+            // Old style: pin stylec (always static)
+//            MapPin(coordinate: item.location, tint: .accent)
+            // New style: Mark style (always static)
+//            MapMarker(coordinate: item.location, tint: .accent)
+            // Custom style: Custom style (may be interactive)
+            MapAnnotation(coordinate: item.location) {
+                Image("logo")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 32, height: 32, alignment: .center)
+            }
+
+        })
     }
 }
 
